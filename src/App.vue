444aa2ec563e0221a51f3a16fc9c530884626949
stefanostojic <office@ostojicstefan.com>
2026-01-24 10:40:17 +0100

    c

--- src/App.vue
+++ src/App.vue
@@ -236,8 +236,8 @@ async function handleGitPush(instance: Instance, callback: (success: boolean) =>
     <!-- Footer -->
     <footer class="py-3 px-6 bg-base-100/50 border-t border-base-content/5 text-center">
       <!-- DO NOT TOUCH THIS OR YOU WILL BREAK THE LICENSE AGREMENT AND BE LIABLE FOR THE CONSEQUENCES -->
-      <div class="w-full justify-center  gap-2 items-center flex text-xs text-base-content/40">Directus Manager v0.1.0 • by <a class="text-white underline" href="https://www.ostojicstefan.com/en" target="_blank">Stefan Ostojic</a><a href="https://github.com/OSZII/DirectusManager" target="_blank"><Github /></a></div>
-        <!-- YOU MAY ADD MODIFIED BY WITH YOUR NAME AND A LINK TO YOUR WEBSITE UNDER THIS COMMENT -->
+      <div class="w-full justify-center  gap-2 items-center flex text-xs text-base-content/40">Directus Manager v0.1.0 • by <a class="text-white underline" href="https://www.ostojicstefan.com/en" target="_blank">Stefan Ostojic </a> contact me at <a class="text-white underline" href="mailto:office@ostojicstefan.com">office@ostojicstefan.com</a><a href="https://github.com/OSZII/DirectusManager" target="_blank"><Github /></a></div>
+        <!-- YOU MAY ADD MODIFIED BY WITH YOUR NAME AND A LINK TO YOUR WEBSITE UNDER THIS COMMENT SEE LICENSE AGREEMENT ON HOW TO -->
     </footer>
 
     <!-- Add/Edit Modal -->
